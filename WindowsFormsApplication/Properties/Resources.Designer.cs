b8040a0df01041c7f87f499e34f242b1839843fb
capybara1 <capybara1@users.noreply.github.com>
2020-08-26 17:35:25 +0200

    Update to .NET Framework v4.8

--- WindowsFormsApplication/Properties/Resources.Designer.cs
+++ WindowsFormsApplication/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AsyncAwait.WindowsFormsApplicationApplication.Properties {
+namespace AsyncAwait.WindowsFormsApplication.Properties {
     using System;
     
     
@@ -19,7 +19,7 @@ namespace AsyncAwait.WindowsFormsApplicationApplication.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -39,7 +39,7 @@ namespace AsyncAwait.WindowsFormsApplicationApplication.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AsyncAwait.WindowsFormsApplicationApplication.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AsyncAwait.WindowsFormsApplication.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
